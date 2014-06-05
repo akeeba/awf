a5d44794c34d4cc17a7cdb214721b99a445df2c6
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-06-06 01:13:39 +0300

    Testing the Registry package

---
+++ Tests/Awf/Registry/AbstractRegistryFormatTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package		awf
+ * @copyright	2014 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license		GNU GPL version 3 or later
+ */
+
+namespace Tests\Awf\Registry;
+use Awf\Registry\AbstractRegistryFormat;
+
+/**
+ * @coversDefaultClass Awf\Registry\AbstractRegistryFormat
+ *
+ * @package Tests\Awf\Registry
+ */
+class AbstractRegistryFormatTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Data provider for testGetInstance
+	 *
+	 * @return  array
+	 *
+	 * @since   1.0
+	 */
+	public function seedTestGetInstance()
+	{
+		return array(
+			array('Xml'),
+			array('Ini'),
+			array('Json'),
+			array('Php'),
+		);
+	}
+
+	/**
+	 * Test the AbstractRegistryFormat::getInstance method.
+	 *
+	 * @param   string  $format  The format to load
+	 *
+	 * @return  void
+	 *
+	 * @dataProvider  seedTestGetInstance
+	 * @since         1.0
+	 */
+	public function testGetInstance($format)
+	{
+		$class = '\\Awf\\Registry\\Format\\' . $format;
+
+		$object = AbstractRegistryFormat::getInstance($format);
+		$this->assertThat(
+			$object instanceof $class,
+			$this->isTrue()
+		);
+	}
+
+	/**
+	 * Test getInstance with a non-existent format.
+	 *
+	 * @return  void
+	 *
+	 * @expectedException  \InvalidArgumentException
+	 * @since              1.0
+	 */
+	public function testGetInstanceNonExistent()
+	{
+		AbstractRegistryFormat::getInstance('Troll');
+	}
+}
