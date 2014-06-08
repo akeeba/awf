726d6adc4c6b7cbe6ef28c965476c14e426a5630
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-06-08 20:58:03 +0300

    Files were under wrong directory

---
+++ Tests/Stubs/Fakeapp/Container.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nicholas
+ * Date: 6/6/2014
+ * Time: 4:11 μμ
+ */
+
+namespace Tests\Stubs\Fakeapp;
+
+
+class Container extends \Awf\Container\Container
+{
+	public function __construct(array $values = array())
+	{
+		$defaults = array(
+			'application_name'     => 'Fakeapp',
+			'session_segment_name' => 'fakeapp',
+			'basePath'             => __DIR__,
+			'languagePath'         => __DIR__ . '/../../data/lang'
+		);
+
+		$values = array_merge($defaults, $values);
+
+		return parent::__construct($values);
+	}
+} 
