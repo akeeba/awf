f9b8cc29ce670236be1d7b5afdada5be3aa2c2fc
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-06-11 18:34:40 +0300

    Adding some Composer dependencies (for later user)

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,7 +8,7 @@
  * Bootstrap file for unit testing
  */
 
-// Include the AWF autoloader. Tip: You can put it outside your site's root as well, just edit the path.
+// Include the AWF autoloader.
 if (false == include __DIR__ . '/../Awf/Autoloader/Autoloader.php')
 {
 	echo 'ERROR: AWF Autoloader not found' . PHP_EOL;
@@ -18,6 +18,15 @@ if (false == include __DIR__ . '/../Awf/Autoloader/Autoloader.php')
 
 // Tell the AWF autoloader where to load test classes from (very useful for stubs!)
 \Awf\Autoloader\Autoloader::getInstance()->addMap('Tests\\', __DIR__);
+
+// Include the Composer autoloader.
+if (false == include __DIR__ . '/../vendor/autoload.php')
+{
+	echo 'ERROR: Composer Autoloader not found' . PHP_EOL;
+
+	exit(1);
+}
+
 
 // This is necessary for the session testing
 ini_set('session.use_only_cookies', false);
