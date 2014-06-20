1e1a859b410b07ce096a42f8516296e24fe2e160
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-06-20 17:23:51 +0300

    Testing Event\Observer

---
+++ tests/Event/ObserverTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package		awf
+ * @copyright	2014 Nicholas K. Dionysopoulos / Akeeba Ltd 
+ * @license		GNU GPL version 3 or later
+ */
+
+namespace Awf\Tests\Event;
+
+
+use Awf\Event\Observable;
+use Awf\Tests\Helpers\ApplicationTestCase;
+use Awf\Tests\Helpers\ReflectionHelper;
+use Awf\Tests\Stubs\Event\FirstObserver;
+
+class ObserverTest extends ApplicationTestCase
+{
+	/** @var Observable */
+	private $dispatcher;
+
+	public static $attachArguments = null;
+
+	public function testConstructor()
+	{
+		$dummy = new FirstObserver($this->dispatcher);
+
+		$this->assertEquals($dummy, self::$attachArguments);
+		$this->assertEquals($this->dispatcher, ReflectionHelper::getValue($dummy, 'subject'));
+	}
+
+	public function testGetObservableEvents()
+	{
+		$dummy = new FirstObserver($this->dispatcher);
+
+		$observableEvents = $dummy->getObservableEvents();
+
+		$this->assertEquals(array(
+			'returnConditional',
+			'identifyYourself',
+			'chain',
+		), $observableEvents);
+	}
+
+	protected function setUp()
+	{
+		$this->dispatcher = $this->getMockBuilder('\\Awf\\Event\\Observable')
+			->disableOriginalConstructor()
+			->setMethods(array('attach', 'detach', 'trigger'))
+			->getMock();
+
+		$this->dispatcher
+			->expects($this->any())
+			->method('attach')
+			->will($this->returnCallback(function($arg){
+				ObserverTest::$attachArguments = $arg;
+			}));
+
+		$this->dispatcher
+			->expects($this->any())
+			->method('detach')
+			->willReturnSelf();
+
+		$this->dispatcher
+			->expects($this->any())
+			->method('trigger')
+			->willReturn(array());
+	}
+}
+ 
