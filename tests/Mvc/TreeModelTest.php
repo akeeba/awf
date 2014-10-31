7b485143f73143668b2c28402ae57cd1b15f939a
Davide Tampellini <tampe125@gmail.com>
2014-10-31 17:31:20 +0100

    Added tests for TreeModel::forceDelete (partially)

--- tests/Mvc/TreeModelTest.php
+++ tests/Mvc/TreeModelTest.php
@@ -10,12 +10,14 @@
 
 namespace Awf\Tests\TreeModel;
 
-use Awf\Tests\Database\DatabaseMysqliCase;
+use Awf\Tests\Database\DatabaseMysqlCase;
 use Awf\Database\Driver;
 use Awf\Tests\Stubs\Fakeapp\Container;
 use Awf\Tests\Stubs\Mvc\TreeModelStub;
 
-class TreeModelTest extends DatabaseMysqliCase
+use \Mockery as m;
+
+class TreeModelTest extends DatabaseMysqlCase
 {
     /**
      * @group           TreeModel
@@ -60,6 +62,68 @@ class TreeModelTest extends DatabaseMysqliCase
     }
 
     /**
+     * @group               TreeModelForceDelete
+     * @group               TreeModel
+     * @covers              TreeModel::forceDelete
+     * @dataProvider        getTestForceDelete
+     */
+    public function testForceDelete($test, $check)
+    {
+        $msg = 'TreeModel::forceDelete %s - Case: '.$check['case'];
+
+        $container = new Container(array(
+            'db' => self::$driver,
+            'mvc_config' => array(
+                'autoChecks'  => false,
+                'idFieldName' => 'dbtest_nestedset_id',
+                'tableName'   => '#__dbtest_nestedsets'
+            )
+        ));
+
+        $db = self::$driver;
+
+        //$table = m::mock('FoftestTableNestedset[onBeforeDelete]', array('#__foftest_nestedsets', 'foftest_nestedset_id', &$db, array('_table_class' => 'FoftestTableNestedset')));
+        $table = new TreeModelStub($container);
+
+        /*$table->shouldAllowMockingProtectedMethods()->shouldReceive('onBeforeDelete')->andReturnUsing(function($oid) use($test){
+            // Check if the current node allows delete or not (default: yes)
+            if(isset($test['mock']['before'][$oid]) && !$test['mock']['before'][$oid])
+            {
+                return false;
+            }
+
+            return true;
+        });*/
+
+        if($test['loadid'])
+        {
+            $table->findOrFail($test['loadid']);
+        }
+
+        $return = $table->forceDelete($test['delete']);
+
+        $this->assertInstanceOf('\\Awf\\Mvc\\TreeModel', $return, sprintf($msg, 'Should return an instance of itself'));
+
+        $pk    = $table->getIdFieldName();
+        $query = $db->getQuery(true)->select($pk)->from($table->getTableName());
+        $items = $db->setQuery($query)->loadColumn();
+
+        $this->assertEmpty(array_intersect($check['deleted'], $items), sprintf($msg, 'Faiiled to delete all the items'));
+
+        $query = $db->getQuery(true)
+                    ->select('*')
+                    ->from($table->getTableName())
+                    ->where($db->qn($pk).' IN('.implode(',', array_keys($check['nodes'])).')');
+        $nodes = $db->setQuery($query)->loadObjectList();
+
+        foreach($nodes as $node)
+        {
+            $this->assertEquals($check['nodes'][$node->$pk]['lft'], $node->lft, sprintf($msg, 'Failed to update the lft value of the node with id '.$node->$pk));
+            $this->assertEquals($check['nodes'][$node->$pk]['rgt'], $node->rgt, sprintf($msg, 'Failed to update the rgt value of the node with id '.$node->$pk));
+        }
+    }
+
+    /**
      * @group               TreeModelReorder
      * @group               TreeModel
      * @covers              TreeModel::reorder
@@ -72,7 +136,7 @@ class TreeModelTest extends DatabaseMysqliCase
             'db' => self::$driver,
             'mvc_config' => array(
                 'autoChecks'  => false,
-                'idFieldName' => 'id',
+                'idFieldName' => 'dbtest_nestedset_id',
                 'tableName'   => '#__dbtest_nestedsets'
             )
         ));
@@ -94,7 +158,7 @@ class TreeModelTest extends DatabaseMysqliCase
             'db' => self::$driver,
             'mvc_config' => array(
                 'autoChecks'  => false,
-                'idFieldName' => 'id',
+                'idFieldName' => 'dbtest_nestedset_id',
                 'tableName'   => '#__dbtest_nestedsets'
             )
         ));
@@ -188,6 +252,150 @@ class TreeModelTest extends DatabaseMysqliCase
         $table->insertAsRoot();
     }
 
+    public function getTestForceDelete()
+    {
+        /*
+         * At the moment I can only test when onBeforeDelete return false in the first level only.
+         * That's because the iterator is spawning a new class every time, so the mock we setup is not used
+         * and the check if performed vs the "real" object, which of course returns false.
+         */
+
+        // Delete a single leaf item
+        $data[] = array(
+            array(
+                'loadid'    => null,
+                'delete'    => 15,
+                'mock'      => array(
+                    'before'    => array()
+                )
+            ),
+            array(
+                'case' => 'Delete a single leaf item',
+                'deleted' => array(15),
+                // Associative array where the index is the node id, so I can double check if the lft rgt values
+                // are correctly updated
+                'nodes'   => array(
+                    1  => array('lft' => 1, 'rgt' => 30),
+                    9  => array('lft' => 16, 'rgt' => 29),
+                    14 => array('lft' => 25, 'rgt' => 28)
+                )
+            )
+        );
+
+        // Delete a single leaf item (loaded table)
+        $data[] = array(
+            array(
+                'loadid'    => 15,
+                'delete'    => null,
+                'mock'      => array(
+                    'before'    => array()
+                )
+            ),
+            array(
+                'case' => 'Delete a single leaf item (loaded table)',
+                'deleted' => array(15),
+                // Associative array where the index is the node id, so I can double check if the lft rgt values
+                // are correctly updated
+                'nodes'   => array(
+                    1  => array('lft' => 1, 'rgt' => 30),
+                    9  => array('lft' => 16, 'rgt' => 29),
+                    14 => array('lft' => 25, 'rgt' => 28)
+                )
+            )
+        );
+
+        // Delete a single leaf item - prevented
+        /*$data[] = array(
+            array(
+                'loadid'    => null,
+                'delete'    => 15,
+                'mock'      => array(
+                    'before'    => array(
+                        15 => false
+                    )
+                )
+            ),
+            array(
+                'case' => 'Delete a single leaf item - prevented',
+                'deleted' => array(),
+                // Associative array where the index is the node id, so I can double check if the lft rgt values
+                // are correctly updated
+                'nodes'   => array(
+                    1  => array('lft' => 1, 'rgt' => 32),
+                    9  => array('lft' => 16, 'rgt' => 31),
+                    14 => array('lft' => 25, 'rgt' => 30)
+                )
+            )
+        );*/
+
+        // Delete a single trunk item
+        $data[] = array(
+            array(
+                'loadid'    => null,
+                'delete'    => 14,
+                'mock'      => array(
+                    'before'    => array()
+                )
+            ),
+            array(
+                'case' => 'Delete a single trunk item',
+                'deleted' => array(14, 15, 16),
+                // Associative array where the index is the node id, so I can double check if the lft rgt values
+                // are correctly updated
+                'nodes'   => array(
+                    1 => array('lft' =>  1, 'rgt' => 26),
+                    9 => array('lft' => 16, 'rgt' => 25)
+                )
+            )
+        );
+
+        // Delete a single trunk item (loaded table)
+        $data[] = array(
+            array(
+                'loadid'    => 14,
+                'delete'    => null,
+                'mock'      => array(
+                    'before'    => array()
+                )
+            ),
+            array(
+                'case' => 'Delete a single trunk item (loaded table)',
+                'deleted' => array(14, 15, 16),
+                // Associative array where the index is the node id, so I can double check if the lft rgt values
+                // are correctly updated
+                'nodes'   => array(
+                    1 => array('lft' =>  1, 'rgt' => 26),
+                    9 => array('lft' => 16, 'rgt' => 25)
+                )
+            )
+        );
+
+        // Delete a single trunk item - prevented
+        /*$data[] = array(
+            array(
+                'loadid'    => null,
+                'delete'    => 14,
+                'mock'      => array(
+                    'before'    => array(
+                        14 => false
+                    )
+                )
+            ),
+            array(
+                'case' => 'Delete a single trunk item - prevented',
+                'deleted' => array(),
+                // Associative array where the index is the node id, so I can double check if the lft rgt values
+                // are correctly updated
+                'nodes'   => array(
+                    1 => array('lft' =>  1, 'rgt' => 32),
+                    9 => array('lft' => 16, 'rgt' => 31)
+                )
+            )
+        );*/
+
+        return $data;
+    }
+
     public function getTestCheck()
     {
         $data[] = array(
@@ -244,7 +452,7 @@ class TreeModelTest extends DatabaseMysqliCase
         return $data;
     }
 
-    public static function getTestCreate()
+    public function getTestCreate()
     {
         // Create a node under the root
         $data[] = array(
