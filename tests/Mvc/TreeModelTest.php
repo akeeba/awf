f1ad9af698eb5225e72d97b06722eb0350fe077b
Davide Tampellini <tampe125@gmail.com>
2014-11-03 12:16:46 +0100

    Added tests for TreeModel::insertAsFirstChildOf

--- tests/Mvc/TreeModelTest.php
+++ tests/Mvc/TreeModelTest.php
@@ -249,6 +249,101 @@ class TreeModelTest extends DatabaseMysqlCase
         $table->insertAsRoot();
     }
 
+    /**
+     * @group               TreeModelInsertAsFirstChildOf
+     * @group               TreeModel
+     * @covers              TreeModel::insertAsFirstChildOf
+     * @dataProvider        getTestInsertAsFirstChildOf
+     */
+    public function testInsertAsFirstChildOf($test, $check)
+    {
+        /** @var TreeModelStub $table */
+        /** @var TreeModelStub $parent */
+
+        $msg = 'TreeModel::insertAsFirstChildOf %s - Case: '.$check['case'];
+        $db  = self::$driver;
+
+        $container = new Container(array(
+            'db' => self::$driver,
+            'mvc_config' => array(
+                'autoChecks'  => false,
+                'idFieldName' => 'dbtest_nestedset_id',
+                'tableName'   => '#__dbtest_nestedsets'
+            )
+        ));
+
+        $table  = new TreeModelStub($container);
+        $parent = $table->getClone();
+
+        if($test['loadid'])
+        {
+            $table->findOrFail($test['loadid']);
+        }
+
+        if($test['title'])
+        {
+            $table->title = $test['title'];
+        }
+
+        $parent->findOrFail($test['parentid']);
+        $parentLft = $parent->lft;
+        $parentRgt = $parent->rgt;
+
+        $return = $table->insertAsFirstChildOf($parent);
+
+        $this->assertInstanceOf('\\Awf\\Mvc\\TreeModel', $return, sprintf($msg, 'Should return an instance of itself'));
+
+        // Assertions on the objects
+        $this->assertNotEquals($test['loadid'], $table->getId(), sprintf($msg, 'Should always create a new node'));
+
+        $this->assertEquals($parentLft, $parent->lft, sprintf($msg, 'Should not touch the lft value of the parent'));
+        $this->assertEquals($parentRgt + 2, $parent->rgt, sprintf($msg, 'Should increase the rgt value by 2'));
+        $this->assertEquals(1, $table->rgt - $table->lft, sprintf($msg, 'Should insert the node as leaf'));
+        $this->assertEquals(1, $table->lft - $parent->lft, sprintf($msg, 'Should insert the node as first child'));
+
+        // Great, the returned objects are ok, what about the ACTUAL data saved inside the db?
+        $query = $db->getQuery(true)
+                    ->select('*')
+                    ->from('#__dbtest_nestedsets')
+                    ->where('dbtest_nestedset_id = '.$table->dbtest_nestedset_id);
+        $nodeDb = $db->setQuery($query)->loadObject();
+
+        $query = $db->getQuery(true)
+                    ->select('*')
+                    ->from('#__dbtest_nestedsets')
+                    ->where('dbtest_nestedset_id = '.$parent->dbtest_nestedset_id);
+        $parentDb = $db->setQuery($query)->loadObject();
+
+        $this->assertEquals($table->lft, $nodeDb->lft, sprintf($msg, 'Children object and database lft values are not the same'));
+        $this->assertEquals($table->rgt, $nodeDb->rgt, sprintf($msg, 'Children object and database rgt values are not the same'));
+        $this->assertEquals($parent->lft, $parentDb->lft, sprintf($msg, 'Parent object and database lft values are not the same'));
+        $this->assertEquals($parent->rgt, $parentDb->rgt, sprintf($msg, 'Parent object and database rgt values are not the same'));
+    }
+
+    /**
+     * @group               TreeModelInsertAsFirstChildOf
+     * @group               TreeModel
+     * @covers              TreeModel::insertAsFirstChildOf
+     */
+    public function testInsertAsFirstChildOfException()
+    {
+        $this->setExpectedException('RuntimeException');
+
+        $container = new Container(array(
+            'db' => self::$driver,
+            'mvc_config' => array(
+                'autoChecks'  => false,
+                'idFieldName' => 'dbtest_nestedset_id',
+                'tableName'   => '#__dbtest_nestedsets'
+            )
+        ));
+
+        $table  = new TreeModelStub($container);
+        $parent = $table->getClone();
+
+        $table->insertAsFirstChildOf($parent);
+    }
+
     public function getTestForceDelete()
     {
         /*
@@ -486,6 +581,59 @@ class TreeModelTest extends DatabaseMysqlCase
                 'data'   => array(
                     'title' => 'Created node'
                 )
+            )
+        );
+
+        return $data;
+    }
+
+    public function getTestInsertAsFirstChildOf()
+    {
+        // Creating a new node
+        $data[] = array(
+            array(
+                'loadid'   => 0,
+                'parentid' => 14,
+                'title'    => 'First child'
+            ),
+            array(
+                'case' => 'Creating a new node'
+            )
+        );
+
+        // Copying an existing node of the same parent (it's not the first child)
+        $data[] = array(
+            array(
+                'loadid'   => 16,
+                'parentid' => 14,
+                'title'    => ''
+            ),
+            array(
+                'case' => "Copying an existing node of the same parent (it's not the first child)"
+            )
+        );
+
+        // Copying an existing node of the same parent (it's the first child)
+        $data[] = array(
+            array(
+                'loadid'   => 15,
+                'parentid' => 14,
+                'title'    => ''
+            ),
+            array(
+                'case' => "Copying an existing node of the same parent (it's the first child)"
+            )
+        );
+
+        // Copying an existing node of another parent
+        $data[] = array(
+            array(
+                'loadid'   => 4,
+                'parentid' => 14,
+                'title'    => ''
+            ),
+            array(
+                'case' => 'Copying an existing node of another parent'
             )
         );
 
