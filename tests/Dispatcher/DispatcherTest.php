7caf2a980f5630aaa81da4cd2d6167213269d33f
Davide Tampellini <tampe125@gmail.com>
2014-11-10 12:26:57 +0100

    Fixed tests failing due to shared input data in the container

--- tests/Dispatcher/DispatcherTest.php
+++ tests/Dispatcher/DispatcherTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package		awf
- * @copyright	2014 Nicholas K. Dionysopoulos / Akeeba Ltd 
+ * @copyright	2014 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license		GNU GPL version 3 or later
  */
 
@@ -10,12 +10,23 @@ namespace Awf\Tests\Dispatcher;
 
 use Awf\Application\Application;
 use Awf\Dispatcher\Dispatcher;
+use Awf\Input\Input;
 use Fakeapp\Controller\Jasager;
 use Awf\Tests\Helpers\ReflectionHelper;
 use Awf\Tests\Stubs\Fakeapp\Container as FakeContainer;
 
 class DispatcherTest extends \Awf\Tests\Helpers\ApplicationTestCase
 {
+	protected function tearDown()
+	{
+		parent::tearDown();
+		
+		// Since we are manipulating the input, we have to revert it back to empty, otherwise other tests could fail.
+		// To be honest that should not happen, since we are cloning it... however, it IS happening!
+		$container = clone self::$container;
+		$container->input->setData(new Input(array()));
+	}
+
 	public function testGetDispatcherWithoutContainer()
 	{
 		$dispatcher = new Dispatcher();
@@ -192,4 +203,3 @@ class DispatcherTest extends \Awf\Tests\Helpers\ApplicationTestCase
 		$dispatcher->dispatch();
 	}
 }
- 
