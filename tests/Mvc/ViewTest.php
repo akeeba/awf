41622a5b73eae8af17113ce059996be6e595f2f0
Davide Tampellini <tampe125@gmail.com>
2014-11-12 09:38:41 +0100

    Added tests for View::setModel

--- tests/Mvc/ViewTest.php
+++ tests/Mvc/ViewTest.php
@@ -9,6 +9,7 @@ namespace Awf\Tests\View;
 
 use Awf\Tests\Helpers\ReflectionHelper;
 use Awf\Tests\Stubs\Fakeapp\Container;
+use Awf\Tests\Stubs\Mvc\ModelStub;
 use Awf\Tests\Stubs\Mvc\ViewStub;
 
 require_once 'ViewDataprovider.php';
@@ -26,5 +27,22 @@ class ViewTest extends \PHPUnit_Framework_TestCase
         $escape = $view->escape('<>àè?"\'');
 
         $this->assertEquals("&lt;&gt;àè?&quot;'", $escape, 'View::escape Failed to escape the string');
+    }
+
+    /**
+     * @group           View
+     * @group           ViewSetModel
+     * @covers          View::setModel
+     */
+    public function testSetModel()
+    {
+        $model      = new ModelStub();
+        $controller = new ViewStub();
+        $controller->setModel('foobar', $model);
+
+        $models = ReflectionHelper::getValue($controller, 'modelInstances');
+
+        $this->assertArrayHasKey('foobar', $models, 'View::setModel Failed to save the model');
+        $this->assertSame($model, $models['foobar'], 'View::setModel Failed to store the same copy of the passed model');
     }
 }
