39ccf653b028c99de23e2bda8583f7f91a880edd
Davide Tampellini <tampe125@gmail.com>
2014-11-19 11:54:53 +0100

    Added tests for DataModel::reset

--- tests/Mvc/DataModelDataprovider.php
+++ tests/Mvc/DataModelDataprovider.php
@@ -102,4 +102,177 @@ class DataModelDataprovider
 
         return $data;
     }
+
+    public static function getTestReset()
+    {
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array('id' => null, 'title' => null, 'start_date' => null, 'description' => null),
+                    'eagerRelations'  => array(),
+                    'relationFilters' => array()
+                ),
+                'table'     => '#__dbtest',
+                'default'   => true,
+                'relations' => false
+            ),
+            array(
+                'case'           => 'Table with no defaults, no relations nor filters. Resetting to default, not resetting the relations',
+                'resetRelations' => false,
+                'eager'          => array(),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => null,
+                    'start_date'  => null,
+                    'description' => null
+                )
+            )
+        );
+
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array('id' => null, 'title' => null, 'start_date' => null, 'description' => null, 'foobar' => 'test'),
+                    'eagerRelations'  => array(),
+                    'relationFilters' => array()
+                ),
+                'table'     => '#__dbtest',
+                'default'   => true,
+                'relations' => false
+            ),
+            array(
+                'case'           => 'Table with no defaults, no relations nor filters. Resetting to default, not resetting the relations. Additional fields set',
+                'resetRelations' => false,
+                'eager'          => array(),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => null,
+                    'start_date'  => null,
+                    'description' => null
+                )
+            )
+        );
+
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array('id' => null, 'title' => null, 'start_date' => null, 'description' => null),
+                    'eagerRelations'  => array(),
+                    'relationFilters' => array()
+                ),
+                'table'     => '#__dbtest',
+                'default'   => false,
+                'relations' => false
+            ),
+            array(
+                'case'           => 'Table with no defaults, no relations nor filters. Not resetting to default, not resetting the relations',
+                'resetRelations' => false,
+                'eager'          => array(),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => null,
+                    'start_date'  => null,
+                    'description' => null
+                )
+            )
+        );
+
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array('id' => null, 'title' => null, 'start_date' => null, 'description' => null),
+                    'eagerRelations'  => array(),
+                    'relationFilters' => array()
+                ),
+                'table'     => '#__dbtest_defaults',
+                'default'   => true,
+                'relations' => false
+            ),
+            array(
+                'case'           => 'Table with defaults, no relations nor filters. Resetting to defaults, not resetting the relations',
+                'resetRelations' => false,
+                'eager'          => array(),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => 'dummy',
+                    'start_date'  => '0000-00-00 00:00:00',
+                    'description' => null
+                )
+            )
+        );
+
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array('id' => null, 'title' => null, 'start_date' => null, 'description' => null),
+                    'eagerRelations'  => array(),
+                    'relationFilters' => array()
+                ),
+                'table'     => '#__dbtest_defaults',
+                'default'   => false,
+                'relations' => false
+            ),
+            array(
+                'case'           => 'Table with defaults, no relations nor filters. Not resetting to defaults, not resetting the relations',
+                'resetRelations' => false,
+                'eager'          => array(),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => null,
+                    'start_date'  => null,
+                    'description' => null
+                )
+            )
+        );
+
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array(),
+                    'eagerRelations'  => array('foo' => 'bar'),
+                    'relationFilters' => array('dummy')
+                ),
+                'table'     => '#__dbtest',
+                'default'   => true,
+                'relations' => false
+            ),
+            array(
+                'case'           => 'Relations set, but we are not resetting them',
+                'resetRelations' => false,
+                'eager'          => array('foo' => 'bar'),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => null,
+                    'start_date'  => null,
+                    'description' => null
+                )
+            )
+        );
+
+        $data[] = array(
+            array(
+                'mock' => array(
+                    'recordData'      => array(),
+                    'eagerRelations'  => array('foo' => 'bar'),
+                    'relationFilters' => array('dummy')
+                ),
+                'table'     => '#__dbtest',
+                'default'   => true,
+                'relations' => true
+            ),
+            array(
+                'case'           => 'Relations set, we are resetting them',
+                'resetRelations' => true,
+                'eager'          => array(),
+                'data'           => array(
+                    'id'          => null,
+                    'title'       => null,
+                    'start_date'  => null,
+                    'description' => null
+                )
+            )
+        );
+
+        return $data;
+    }
 }
--- tests/Mvc/DataModelTest.php
+++ tests/Mvc/DataModelTest.php
@@ -51,4 +51,45 @@ class DataModeltest extends DatabaseMysqliCase
 
         $this->assertEquals($check['count'], $count, sprintf($msg, 'Called the magic setter the wrong amount of times'));
     }
+
+    /**
+     * @group           DataModel
+     * @group           DataModelReset
+     * @covers          DataModel::reset
+     * @dataProvider    DataModelDataprovider::getTestReset
+     */
+    public function testReset($test, $check)
+    {
+        $msg = 'DataModel::reset %s - Case: '.$check['case'];
+
+        $container = new Container(array(
+            'db' => self::$driver,
+            'mvc_config' => array(
+                'autoChecks'  => false,
+                'idFieldName' => 'id',
+                'tableName'   => $test['table']
+            )
+        ));
+
+        $model = new DataModelStub($container);
+
+        $relation = $this->getMock('\\Awf\\Mvc\\DataModel\\RelationManager', array('resetRelations'), array($model));
+        $relation->expects($check['resetRelations'] ? $this->once() : $this->never())->method('resetRelations')->willReturn(null);
+
+        ReflectionHelper::setValue($model, 'relationManager', $relation);
+        ReflectionHelper::setValue($model, 'recordData', $test['mock']['recordData']);
+        ReflectionHelper::setValue($model, 'eagerRelations', $test['mock']['eagerRelations']);
+        ReflectionHelper::setValue($model, 'relationFilters', $test['mock']['relationFilters']);
+
+        $return = $model->reset($test['default'], $test['relations']);
+
+        $data    = ReflectionHelper::getValue($model, 'recordData');
+        $eager   = ReflectionHelper::getValue($model, 'eagerRelations');
+        $filters = ReflectionHelper::getValue($model, 'relationFilters');
+
+        $this->assertInstanceOf('\\Awf\\Mvc\\DataModel', $return, sprintf($msg, 'Should return an instance of itself'));
+        $this->assertEquals($check['data'], $data, sprintf($msg, 'Failed to reset the internal data'));
+        $this->assertEquals($check['eager'], $eager, sprintf($msg, 'Eager relations are not correctly set'));
+        $this->assertEmpty($filters, sprintf($msg, 'Relations filters should be empty'));
+    }
 }
