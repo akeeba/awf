94ab1118210ff448cd61be227bf714764418bb5c
Davide Tampellini <tampe125@gmail.com>
2014-12-04 17:03:24 +0100

    Added tables for relation testing

--- tests/Stubs/schema/mysql.sql
+++ tests/Stubs/schema/mysql.sql
@@ -9,3 +9,9 @@ CREATE TABLE `#__dbtest_modifiedon` (  `id` int(10) unsigned NOT NULL AUTO_INCRE
 CREATE TABLE `#__dbtest_alias` (  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,  `title` varchar(50) NOT NULL,  `start_date` datetime NOT NULL,  `description` text NOT NULL, `xx_ordering` TINYINT NOT NULL DEFAULT '0', `xx_enabled` TINYINT NOT NULL DEFAULT '0', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE TABLE `#__dbtest_nestedsets` (`dbtest_nestedset_id` int(11) unsigned NOT NULL AUTO_INCREMENT, `title` varchar(255) NOT NULL DEFAULT '', `slug` varchar(255) NOT NULL DEFAULT '', `lft` int(11) DEFAULT NULL, `rgt` int(11) DEFAULT NULL, `hash` char(40) DEFAULT NULL,  PRIMARY KEY (`dbtest_nestedset_id`),  KEY `lft` (`lft`),  KEY `rgt` (`rgt`),  KEY `lft_2` (`lft`,`rgt`),  KEY `char` (`hash`)) DEFAULT CHARSET=utf8;
 CREATE TABLE `#__dbtest_nestedbares` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT,  `title` varchar(255) NOT NULL DEFAULT '',  `lft` int(11) DEFAULT NULL,  `rgt` int(11) DEFAULT NULL,  PRIMARY KEY (`id`),  KEY `lft` (`lft`),  KEY `rgt` (`rgt`),  KEY `lft_2` (`lft`,`rgt`)) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__fakeapp_parents` (`fakeapp_parent_id` INT NOT NULL AUTO_INCREMENT,`dummy` varchar (50) NOT NULL , PRIMARY KEY (`fakeapp_parent_id`));
+CREATE TABLE IF NOT EXISTS `#__fakeapp_children` (`fakeapp_child_id` INT NOT NULL AUTO_INCREMENT, `dummy` varchar (50) NOT NULL , `fakeapp_parent_id` INT NOT NULL , PRIMARY KEY (`fakeapp_child_id`));
+CREATE TABLE IF NOT EXISTS `#__fakeapp_parts`(`fakeapp_part_id` INT NOT NULL AUTO_INCREMENT , `dummy` varchar (50) NOT NULL , PRIMARY KEY (`fakeapp_part_id`));
+CREATE TABLE IF NOT EXISTS `#__fakeapp_groups`( `fakeapp_group_id` INT NOT NULL AUTO_INCREMENT , `dummy` varchar (50) NOT NULL , PRIMARY KEY (`fakeapp_group_id`));
+CREATE TABLE IF NOT EXISTS `#__fakeapp_parts_groups`(`fakeapp_group_id` INT NOT NULL , `fakeapp_part_id` INT NOT NULL);
