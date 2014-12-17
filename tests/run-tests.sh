81eb167cdc187ebde3f5d0036bd502d45a394b06
Davide Tampellini <tampe125@gmail.com>
2014-12-17 11:51:11 +0100

    Updated the script file so you can pass a seed

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-../vendor/bin/phpunit-randomizer -c ../phpunit.xml ../tests/
+
+if [ "$1" ]
+then
+	../vendor/bin/phpunit-randomizer --seed $1 -c ../phpunit.xml ../tests/
+else
+	../vendor/bin/phpunit-randomizer -c ../phpunit.xml ../tests/
+fi
