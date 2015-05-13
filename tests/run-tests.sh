c6d71ee6684605b2c6675ab55e6edf474a71008a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2015-05-13 10:40:12 +0300

    Do not randomize the tests

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ "$1" ]
-then
-	../vendor/bin/phpunit-randomizer --seed $1 -c ../phpunit.xml ../tests/
-else
-	../vendor/bin/phpunit-randomizer -c ../phpunit.xml ../tests/
-fi
+../vendor/bin/phpunit -c ../phpunit.xml ../tests/ ${@}
