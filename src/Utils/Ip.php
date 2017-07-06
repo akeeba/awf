64cb1acd41c86a813cc7bd3b8fc8d5e624914af5
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-07-06 13:50:20 +0300

    Add support for Sucuri firewall HTTP header
    See akeeba/admintools#128

--- src/Utils/Ip.php
+++ src/Utils/Ip.php
@@ -63,6 +63,12 @@ class Ip
 				return $_SERVER['HTTP_X_FORWARDED_FOR'];
 			}
 
+			// Are we using Sucuri firewall? They use a custom HTTP header
+			if (array_key_exists('HTTP_X_SUCURI_CLIENTIP', $_SERVER) && !empty($_SERVER['HTTP_X_SUCURI_CLIENTIP']))
+			{
+				return $_SERVER['HTTP_X_SUCURI_CLIENTIP'];
+			}
+
 			// Do we have a client-ip header (e.g. non-transparent proxy)?
 			if (array_key_exists('HTTP_CLIENT_IP', $_SERVER) && !empty($_SERVER['HTTP_CLIENT_IP']))
 			{
@@ -89,6 +95,12 @@ class Ip
 		if (getenv('HTTP_X_FORWARDED_FOR'))
 		{
 			return getenv('HTTP_X_FORWARDED_FOR');
+		}
+
+		// Are we using Sucuri firewall? They use a custom HTTP header
+		if (getenv('HTTP_X_SUCURI_CLIENTIP'))
+		{
+			return getenv('HTTP_X_SUCURI_CLIENTIP');
 		}
 
 		// Do we have a client-ip header?
