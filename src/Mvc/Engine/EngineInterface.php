1c7e92e2914447465171d7cedbbbbd412c8568f2
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-04-05 00:35:08 +0300

    Add support for Blade templates
    View engine interface

---
+++ src/Mvc/Engine/EngineInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Awf
+ * @copyright Copyright (c)2014-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license     GNU GPL version 3 or later
+ */
+
+namespace Awf\Mvc\Engine;
+
+use Awf\Mvc\View;
+
+interface EngineInterface
+{
+	/**
+	 * Public constructor
+	 *
+	 * @param   View  $view  The view we belong to
+	 */
+	public function __construct(View $view);
+
+	/**
+	 * Get the include path for a parsed view template
+	 *
+	 * @param   string  $path         The path to the view template
+	 * @param   array   $forceParams  Any additional information to pass to the view template engine
+	 *
+	 * @return  array  Content 3ναlυα+ιοη information ['type' => 'raw|path', 'content' => 'path or raw content'] (I use leetspeak here because of bad quality hosts with broken scanners)
+	 */
+	public function get($path, array $forceParams = array());
+}
