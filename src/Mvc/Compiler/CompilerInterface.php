1e26fd86712a05d0ac7010022a3782f9ff571b6d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-04-05 00:34:49 +0300

    Add support for Blade templates
    Add the Blade compiler

---
+++ src/Mvc/Compiler/CompilerInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     Awf
+ * @copyright Copyright (c)2014-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license     GNU GPL version 3 or later
+ */
+
+namespace Awf\Mvc\Compiler;
+
+interface CompilerInterface
+{
+	/**
+	 * Are the results of this compiler engine cacheable? If the engine makes use of the forcedParams it must return
+	 * false.
+	 *
+	 * @return  mixed
+	 */
+	public function isCacheable();
+
+	/**
+	 * Compile a view template into PHP and HTML
+	 *
+	 * @param   string  $path         The absolute filesystem path of the view template
+	 * @param   array   $forceParams  Any parameters to force (only for engines returning raw HTML)
+	 *
+	 * @return mixed
+	 */
+	public function compile($path, array $forceParams = array());
+
+	/**
+	 * Returns the file extension supported by this compiler
+	 *
+	 * @return  string
+	 */
+	public function getFileExtension();
+}
