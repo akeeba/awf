2827ba1e35349607b0bdcc4c734295061b55fc34
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-11-27 17:20:42 +0200

    # [HIGH] Download: Some cURL versions return lowercase headers, making redirection detection impossible

--- src/Download/Adapter/Curl.php
+++ src/Download/Adapter/Curl.php
@@ -14,6 +14,8 @@ use Awf\Text\Text;
  */
 class Curl extends AbstractAdapter implements DownloadInterface
 {
+	protected $headers = array();
+
 	public function __construct()
 	{
 		$this->priority              = 110;
@@ -73,6 +75,7 @@ class Curl extends AbstractAdapter implements DownloadInterface
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($ch, CURLOPT_SSLVERSION, 0);
         curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/cacert.pem');
+		curl_setopt($ch, CURLOPT_HEADERFUNCTION, array($this, 'reponseHeaderCallback'));
 
 		if (!(empty($from) && empty($to)))
 		{
@@ -138,6 +141,10 @@ class Curl extends AbstractAdapter implements DownloadInterface
 		{
 			$error = Text::sprintf('AWF_DOWNLOAD_ERR_LIB_CURL_ERROR', $errno, $errmsg);
 		}
+		elseif (($http_status >= 300) && ($http_status <= 399) && isset($this->headers['location']) && !empty($this->headers['location']))
+		{
+			return $this->downloadAndReturn($this->headers['location'], $from, $to, $params);
+		}
 		elseif ($http_status > 299)
 		{
 			$result = false;
@@ -170,11 +177,17 @@ class Curl extends AbstractAdapter implements DownloadInterface
 
 		$ch = curl_init();
 
+		curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($ch, CURLOPT_SSLVERSION, 0);
+
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_NOBODY, true );
 		curl_setopt($ch, CURLOPT_HEADER, true );
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
 		@curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+		curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/cacert.pem');
 
 		$data = curl_exec($ch);
 		curl_close($ch);
@@ -183,23 +196,73 @@ class Curl extends AbstractAdapter implements DownloadInterface
 		{
 			$content_length = "unknown";
 			$status = "unknown";
+			$redirection = null;
 
-			if (preg_match( "/^HTTP\/1\.[01] (\d\d\d)/", $data, $matches))
+			if (preg_match( "/^HTTP\/1\.[01] (\d\d\d)/i", $data, $matches))
 			{
 				$status = (int)$matches[1];
 			}
 
-			if (preg_match( "/Content-Length: (\d+)/", $data, $matches))
+			if (preg_match( "/Content-Length: (\d+)/i", $data, $matches))
 			{
 				$content_length = (int)$matches[1];
+			}
+
+			if (preg_match( "/Location: (.*)/i", $data, $matches))
+			{
+				$redirection = (int)$matches[1];
 			}
 
 			if( $status == 200 || ($status > 300 && $status <= 308) )
 			{
 				$result = $content_length;
 			}
+
+			if (($status > 300) && ($status <= 308))
+			{
+				if (!empty($redirection))
+				{
+					return $this->getFileSize($redirection);
+				}
+
+				return -1;
+			}
 		}
 
 		return $result;
+	}
+
+	/**
+	 * Handles the HTTP headers returned by cURL
+	 *
+	 * @param   resource  $ch    cURL resource handle (unused)
+	 * @param   string    $data  Each header line, as returned by the server
+	 *
+	 * @return  int  The length of the $data string
+	 */
+	protected function reponseHeaderCallback(&$ch, &$data)
+	{
+		$strlen = strlen($data);
+
+		if (($strlen) <= 2)
+		{
+			return $strlen;
+		}
+
+		if (substr($data, 0, 4) == 'HTTP')
+		{
+			return $strlen;
+		}
+
+		if (strpos($data, ':') === false)
+		{
+			return $strlen;
+		}
+
+		list($header, $value) = explode(': ', trim($data), 2);
+
+		$this->headers[strtolower($header)] = $value;
+
+		return $strlen;
 	}
 }
