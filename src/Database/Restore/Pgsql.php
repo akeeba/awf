d3194a434bc7712134d8ada5a51709f6d7cf3a39
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-03-04 13:51:31 +0200

    Add PDO PostgreSQL support

---
+++ src/Database/Restore/Pgsql.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package    awf
+ * @copyright  Copyright (c)2014-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license    GNU GPL version 3 or later
+ */
+
+namespace Awf\Database\Restore;
+
+
+class Pgsql extends Postgresql
+{
+
+}
