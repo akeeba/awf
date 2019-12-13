612cac3fe58d58d81c9df0b47fdcd708dcaa391a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-12-13 12:21:25 +0200

    Normalize copyright

--- src/Mailer/phpmailer/class.smtp.php
+++ src/Mailer/phpmailer/class.smtp.php
@@ -1,5 +1,11 @@
 <?php
 /**
+ * @package   awf
+ * @copyright Copyright (c)2014-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU GPL version 3 or later
+ */
+
+/**
  * PHPMailer RFC821 SMTP email transport class.
  * PHP Version 5
  * @package PHPMailer
