85c3bcd7839fcf0467b27643b3144c3a85d91ca6
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-02 12:09:15 +0200

    Updating copyright. Happy New Year!

--- tests/Stubs/schema/mysql.sql
+++ tests/Stubs/schema/mysql.sql
@@ -1,5 +1,17 @@
 /*
  * @package   awf
+ * @copyright Copyright (c)2014-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU GPL version 3 or later
+ */
+
+/*
+ * @package   awf
+ * @copyright Copyright (c)2014-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU GPL version 3 or later
+ */
+
+/*
+ * @package   awf
  * @copyright Copyright (c)2014-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU GPL version 3 or later
  */
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+#
+# @package   awf
+# @copyright Copyright (c)2014-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+# @license   GNU GPL version 3 or later
+#
+
 LC_ALL=C ../vendor/bin/phpunit -c ../phpunit.xml ../tests/ ${@}
