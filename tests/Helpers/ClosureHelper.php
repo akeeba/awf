a37f4ba3351ee0aee206dd7a4962a767bd2ab5a8
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-01-02 15:13:43 +0200

    Update copyright headers. Happy New Year!

--- tests/Helpers/ClosureHelper.php
+++ tests/Helpers/ClosureHelper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   awf
- * @copyright Copyright (c)2014-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @copyright Copyright (c)2014-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU GPL version 3 or later
  */
 namespace Awf\Tests\Helpers;
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#
+##
 # @package   awf
-# @copyright Copyright (c)2014-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+# @copyright Copyright (c)2014-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
 # @license   GNU GPL version 3 or later
 #
 
