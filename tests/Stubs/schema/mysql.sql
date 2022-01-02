d12852986ee3b80fa9875eba9a9973eac27f786a
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-01-02 15:13:43 +0200

    Update copyright headers. Happy New Year!

--- tests/Stubs/schema/mysql.sql
+++ tests/Stubs/schema/mysql.sql
@@ -1,3 +1,9 @@
+/**
+ * @package   awf
+ * @copyright Copyright (c)2014-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU GPL version 3 or later
+ */
+
 /*
  * @package   awf
  * @copyright Copyright (c)2014-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
