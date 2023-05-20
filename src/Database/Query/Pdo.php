888ff5d35b6f3be324ec2455974372eaf94643fe
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-05-20 20:51:45 +0300

    Pdo query didn't work with limit

--- src/Database/Query/Pdo.php
+++ src/Database/Query/Pdo.php
@@ -8,12 +8,65 @@
 namespace Awf\Database\Query;
 
 use Awf\Database\Query;
+use Awf\Database\QueryLimitable;
 
 /**
  * Query Building Class.
  *
  * This class is adapted from the Joomla! Framework
  */
-class Pdo extends Query
+class Pdo extends Query implements QueryLimitable
 {
+	/**
+	 * @var    integer  The offset for the result set.
+	 */
+	protected $offset;
+
+	/**
+	 * @var    integer  The limit for the result set.
+	 */
+	protected $limit;
+
+	/**
+	 * Method to modify a query already in string format with the needed
+	 * additions to make the query limited to a particular number of
+	 * results, or start at a particular offset.
+	 *
+	 * @param   string   $query   The query in string format
+	 * @param   integer  $limit   The limit for the result set
+	 * @param   integer  $offset  The offset for the result set
+	 *
+	 * @return string
+	 *
+	 */
+	public function processLimit($query, $limit, $offset = 0)
+	{
+		if ($limit > 0 || $offset > 0)
+		{
+			$query .= ' LIMIT ' . $offset . ', ' . $limit;
+		}
+
+		return $query;
+	}
+
+	/**
+	 * Sets the offset and limit for the result set, if the database driver supports it.
+	 *
+	 * Usage:
+	 * $query->setLimit(100, 0); (retrieve 100 rows, starting at first record)
+	 * $query->setLimit(50, 50); (retrieve 50 rows, starting at 50th record)
+	 *
+	 * @param   integer  $limit   The limit for the result set
+	 * @param   integer  $offset  The offset for the result set
+	 *
+	 * @return  \Awf\Database\Query  Returns this object to allow chaining.
+	 *
+	 */
+	public function setLimit($limit = 0, $offset = 0)
+	{
+		$this->limit  = (int) $limit;
+		$this->offset = (int) $offset;
+
+		return $this;
+	}
 }
