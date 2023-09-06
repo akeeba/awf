783b4e339b460472507693d7c81ee35a8440965f
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-09-06 16:14:58 +0300

    Helper service

---
+++ src/Helper/AbstractHelper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package   awf
+ * @copyright Copyright (c)2014-2023 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU GPL version 3 or later
+ */
+
+namespace Awf\Helper;
+
+use Awf\Container\ContainerAwareInterface;
+use Awf\Container\ContainerAwareTrait;
+
+class AbstractHelper implements HelperInterface, ContainerAwareInterface
+{
+	use ContainerAwareTrait;
+
+	protected $helperPrefix = '';
+
+	/** @inheritDoc */
+	public function getName(): string
+	{
+		if (empty($this->helperPrefix))
+		{
+			$parts              = explode('\\', get_class($this));
+			$this->helperPrefix = strtolower(array_pop($parts));
+		}
+
+		return $this->helperPrefix;
+	}
+}
