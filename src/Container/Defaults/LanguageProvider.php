ae60b0efe91d5dea692aa72de1dea66b95d9d547
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-11-21 21:49:57 +0200

    Rework language management

---
+++ src/Container/Defaults/LanguageProvider.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package   awf
+ * @copyright Copyright (c)2014-2023 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU GPL version 3 or later
+ */
+
+namespace Awf\Container\Defaults;
+
+use Awf\Container\Container;
+use Awf\Text\Language;
+use Awf\Text\Text;
+
+/**
+ * Application default Language object service provider
+ *
+ * @since   1.2.0
+ */
+class LanguageProvider
+{
+	/**
+	 * Returns the service.
+	 *
+	 * @param   Container  $c  The container calling us
+	 *
+	 * @return  Language  The returned service object
+	 * @since   1.2.0
+	 */
+	public function __invoke(Container $c): Language
+	{
+		Text::setContainer($c);
+
+		$lang = new Language($c);
+
+		$lang->loadLanguage();
+
+		return $lang;
+	}
+
+}
