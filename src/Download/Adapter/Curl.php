cbe20359017f2a848eaa6be856b4484e0ee5e125
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2024-05-04 15:45:00 +0300

    Remove CURLOPT_BINARYTRANSFER
    As per https://php.watch/versions/8.4/CURLOPT_BINARYTRANSFER-deprecated
    it has had no effect since PHP 5.1.2.

--- src/Download/Adapter/Curl.php
+++ src/Download/Adapter/Curl.php
@@ -68,7 +68,6 @@ class Curl extends AbstractAdapter implements DownloadInterface
 
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         @curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
