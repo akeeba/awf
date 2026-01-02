f78077a0571d2ef1ee6b3fdbf4f8b11da7021cd1
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2026-01-02 11:39:26 +0200

    Happy New Year!

--- src/Utils/PhpTokenizer.php
+++ src/Utils/PhpTokenizer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   awf
- * @copyright Copyright (c)2014-2025 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @copyright Copyright (c)2014-2026 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU GPL version 3 or later
  */
 namespace Awf\Utils;
